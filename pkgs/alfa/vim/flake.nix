6a566273115b32de829b2c1701f9daa464bf3f38
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-01 07:04:38 -0400

    update

---
+++ pkgs/alfa/vim/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, nixpkgs-stable }:
+    (utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          packages.default = pkgs.symlinkJoin {
+            name = "vim";
+
+            paths = [
+              pkgs.vim
+            ];
+
+            buildInputs = [
+              pkgs.makeWrapper
+            ];
+
+            postBuild = ''
+            wrapProgram $out/bin/vim \
+              --add-flags '-u "$XDG_CONFIG_HOME/vim/vimrc"' \
+              --prefix PATH : $out/bin
+            '';
+          };
+        }
+      )
+    );
+}
