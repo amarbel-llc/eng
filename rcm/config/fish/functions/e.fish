1702fdc7a2c7d87b6bb5b3ce2d81a44bfe5b69f7
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-07 09:07:29 -0400

    update

--- rcm/config/fish/functions/e.fish
+++ rcm/config/fish/functions/e.fish
@@ -1,3 +1,6 @@
-function e --wraps=editor
+set -l editor_name (basename $EDITOR)
+
+function e --inherit-variable editor_name --wraps=$editor_name
+  printf '\033]2;%s\033\\' $editor_name
   $EDITOR $argv
 end
--- rcm/config/fish/thefuck-config.fish
+++
@@ -1,5 +0,0 @@
-
-# aliasing `thefuck` to `fuck`
-if command -s thefuck > /dev/null
-  eval (thefuck --alias | tr '\n' ';')
-end
