0e7aa14f3ad3c4ca28c5f99f7fbbd3146571baa8
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-07-15 06:22:15 -0400

    update

--- rcm/hooks/post-up/our_direnv.bash
+++ rcm/hooks/post-up/our_direnv.bash
@@ -2,3 +2,13 @@
 
 our_direnv="$(readlink "$HOME/eng/result/bin/direnv")"
 "$our_direnv" hook fish > ~/.config/fish/direnv-config.fish
+
+# format the file
+vim \
+  -e \
+  -s \
+  --clean \
+  -c 'normal gg=G' \
+  -c 'wq' \
+  ~/.config/fish/direnv-config.fish \
+  </dev/null
--- rcm/tag-direnv/config/fish/direnv-config.fish
+++ rcm/tag-direnv/config/fish/direnv-config.fish
@@ -1,24 +1,24 @@
 
-    function __direnv_export_eval --on-event fish_prompt;
-        "/nix/store/vip71i9w458y607adcja01ik8lbq5n4g-direnv-2.36.0/bin/direnv" export fish | source;
+function __direnv_export_eval --on-event fish_prompt;
+	"/nix/store/g0y8q1v5gash235qqbw5187bd4qsp149-direnv-2.37.0/bin/direnv" export fish | source;
 
-        if test "$direnv_fish_mode" != "disable_arrow";
-            function __direnv_cd_hook --on-variable PWD;
-                if test "$direnv_fish_mode" = "eval_after_arrow";
-                    set -g __direnv_export_again 0;
-                else;
-                    "/nix/store/vip71i9w458y607adcja01ik8lbq5n4g-direnv-2.36.0/bin/direnv" export fish | source;
-                end;
-            end;
-        end;
-    end;
+	if test "$direnv_fish_mode" != "disable_arrow";
+		function __direnv_cd_hook --on-variable PWD;
+			if test "$direnv_fish_mode" = "eval_after_arrow";
+				set -g __direnv_export_again 0;
+			else;
+				"/nix/store/g0y8q1v5gash235qqbw5187bd4qsp149-direnv-2.37.0/bin/direnv" export fish | source;
+			end;
+		end;
+	end;
+end;
 
-    function __direnv_export_eval_2 --on-event fish_preexec;
-        if set -q __direnv_export_again;
-            set -e __direnv_export_again;
-            "/nix/store/vip71i9w458y607adcja01ik8lbq5n4g-direnv-2.36.0/bin/direnv" export fish | source;
-            echo;
-        end;
+function __direnv_export_eval_2 --on-event fish_preexec;
+	if set -q __direnv_export_again;
+		set -e __direnv_export_again;
+		"/nix/store/g0y8q1v5gash235qqbw5187bd4qsp149-direnv-2.37.0/bin/direnv" export fish | source;
+		echo;
+	end;
 
-        functions --erase __direnv_cd_hook;
-    end;
+	functions --erase __direnv_cd_hook;
+end;
