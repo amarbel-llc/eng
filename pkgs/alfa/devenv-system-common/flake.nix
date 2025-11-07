5187edb411a333a6750d3b4563673484666ab54c
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-11-07 17:04:53 -0500

    update

--- pkgs/alfa/devenv-system-common/flake.nix
+++ pkgs/alfa/devenv-system-common/flake.nix
@@ -7,107 +7,120 @@
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , nixpkgs-stable
-    , utils
-    , fh
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      utils,
+      fh,
     }:
-    (utils.lib.eachDefaultSystem
-      (system:
+    (utils.lib.eachDefaultSystem (
+      system:
       let
 
-        pkgs = import nixpkgs
-          {
-            inherit system;
-          };
+        pkgs = import nixpkgs {
+          config.allowUnfree = true;
+          inherit system;
+        };
 
-        pkgs-stable = import nixpkgs-stable
-          {
-            inherit system;
-          };
+        pkgs-stable = import nixpkgs-stable {
+          config.allowUnfree = true;
+          inherit system;
+        };
 
       in
       {
         packages = {
-          default = with pkgs; symlinkJoin {
-            failOnMissing = true;
-            name = "system-packages";
-            paths = [
-              age
-              asdf
-              asdf-vm
-              bats
-              coreutils
-              csvkit
-              curl
-              curlftpfs
-              dash
-              ddrescue
-              direnv
-              ffmpeg
-              figlet
-              fish
-              fontconfig
-              fswatch
-              fh
-              gawk
-              pkgs-stable.gftp
-              gh
-              git
-              git-secret
-              glibcLocales
-              gnumake
-              (pkgs.gnupg.override { withPcsc = true; })
-              gnuplot
-              gpgme
-              graphviz
-              helix
-              hostess
-              httpie
-              hub
-              imagemagick
-              isolyzer
-              j2cli
-              jinja2-cli
-              jq
-              just
-              lftp
-              libcdio
-              # moreutils
-              neovim
-              nix-direnv
-              nixpkgs-fmt
-              ocrmypdf
-              pandoc
-              paperkey
-              parallel
-              pay-respects
-              # pcsclite
-              plantuml
-              rcm
-              rsync
-              shellcheck
-              shfmt
-              silver-searcher
-              socat
-              sshpass
-              timidity
-              timg
-              tldr
-              tmux
-              tree
-              uv
-              watchexec
-              websocat
-              wget
-              yubico-piv-tool
-              yt-dlp
-              yubikey-manager
-              zstd
-            ];
-          };
+          default =
+            with pkgs;
+            symlinkJoin {
+              failOnMissing = true;
+              name = "system-packages";
+              paths = [
+                age
+                asdf
+                asdf-vm
+                bats
+                coreutils
+                crush
+                csvkit
+                curl
+                curlftpfs
+                dash
+                ddrescue
+                direnv
+                ffmpeg
+                figlet
+                fish
+                fontconfig
+                fswatch
+                fh
+                freeze
+                gawk
+                pkgs-stable.gftp
+                gh
+                git
+                git-secret
+                glibcLocales
+                glow
+                gnumake
+                (pkgs.gnupg.override { withPcsc = true; })
+                gnuplot
+                gpgme
+                graphviz
+                gum
+                helix
+                hostess
+                httpie
+                hub
+                imagemagick
+                isolyzer
+                j2cli
+                jinja2-cli
+                jq
+                just
+                lftp
+                libcdio
+                markscribe
+                melt
+                mods
+                # moreutils
+                neovim
+                nix-direnv
+                nixpkgs-fmt
+                ocrmypdf
+                pandoc
+                paperkey
+                parallel
+                pay-respects
+                # pcsclite
+                plantuml
+                pop
+                rcm
+                rsync
+                shellcheck
+                shfmt
+                silver-searcher
+                skate
+                socat
+                sshpass
+                timidity
+                timg
+                tldr
+                tmux
+                tree
+                uv
+                vhs
+                watchexec
+                websocat
+                wget
+                yubico-piv-tool
+                yt-dlp
+                yubikey-manager
+                zstd
+              ];
+            };
         };
-      })
-    );
+      }
+    ));
 }
