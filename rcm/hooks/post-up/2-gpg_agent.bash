7873eb23b6b877fa780117fe2d04f0751e7ee19f
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-11-10 18:32:22 -0500

    update

--- rcm/hooks/post-up/2-gpg_agent.bash
+++ rcm/hooks/post-up/2-gpg_agent.bash
@@ -3,10 +3,32 @@
 export PATH="$HOME/eng/result/bin:$PATH"
 eval "$(direnv hook bash)"
 
-if ! fish -c reset-gpg >/dev/null; then
-  gum log -l error "failed to start gpg agent"
-fi
+{
+  output="$(mktemp)"
 
-if ! ssh-agent-mux --restart-service; then
-  gum log -l error "failed to start ssh-agent-mux"
-fi
+  if ! fish -c reset-gpg >"$output" 2>&1; then
+    gum log -l error "failed to start gpg agent:"
+    xargs -L1 gum log -l error <"$output"
+    rm "$output"
+    exit 1
+  fi
+
+  gum log -l info "started gpg agent"
+
+  rm "$output"
+}
+
+{
+  output="$(mktemp)"
+
+  if ! ssh-agent-mux --restart-service >"$output" 2>&1; then
+    gum log -l error "failed to start ssh-agent-mux:"
+    xargs -L1 gum log -l error <"$output"
+    rm "$output"
+    exit 1
+  fi
+
+  gum log -l info "started ssh-agent-mux"
+
+  rm "$output"
+}
