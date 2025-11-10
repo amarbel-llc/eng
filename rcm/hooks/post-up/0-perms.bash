a77ab5102df0b8a7df3e01e732224f2bbe5c3dd7
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-11-10 10:52:39 -0500

    update

--- rcm/hooks/post-up/0-perms.bash
+++ rcm/hooks/post-up/0-perms.bash
@@ -1,9 +1,11 @@
 #! /usr/bin/env -S bash -e
 
-chmod 700 $HOME/.config/ssh
-chmod 700 $HOME/.config/gnupg
+chmod 700 "$HOME"/.config/ssh
+chmod 700 "$HOME"/.config/gnupg
 
-chmod 600 $HOME/.config/ssh/*
-chmod 600 $HOME/.config/gnupg/*
+chmod 600 "$HOME"/.config/ssh/*
+chmod 600 "$HOME"/.config/gnupg/*
 
-chmod 644 $HOME/.config/ssh/*.pub
+# disabled for now because `*.pub` is used in sshconfig `IdentityFile` fields,
+# and OpenSSH treats that as a private key
+# chmod 644 $HOME/.config/ssh/*.pub
