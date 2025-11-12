f9346ba91d95fe9b9683dd0e1b849a61f722e2fe
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-11-12 13:46:16 -0500

    update

--- pkgs/alfa/vim/flake.nix
+++ pkgs/alfa/vim/flake.nix
@@ -7,34 +7,43 @@
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
   };
 
-  outputs = { self, nixpkgs, utils, nixpkgs-stable }:
-    (utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          packages.default = pkgs.symlinkJoin {
-            name = "vim";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      nixpkgs-stable,
+    }:
+    (utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        packages.default = pkgs.symlinkJoin {
+          name = "vim";
 
-            paths = with pkgs;[
-              neovim
-              nodejs
-              tree-sitter
-              vim
-            ];
+          paths = with pkgs; [
+            neovim
+            nodejs
+            tree-sitter
+            vim
+          ];
 
-            buildInputs = [
-              pkgs.makeWrapper
-            ];
+          buildInputs = [
+            pkgs.makeWrapper
+          ];
 
-            postBuild = ''
-              wrapProgram $out/bin/vim \
-                --add-flags '-u "$XDG_CONFIG_HOME/vim/vimrc"' \
-                --prefix PATH : $out/bin
-            '';
-          };
-        }
-      )
-    );
+          postBuild = ''
+            wrapProgram $out/bin/vim \
+              --add-flags '-u "$XDG_CONFIG_HOME/vim/vimrc"' \
+              --prefix PATH : $out/bin
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = builtins.attrValues packages;
+        };
+      }
+    ));
 }
