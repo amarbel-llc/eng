50e3d381a5acbe7dcca39c25222e1bfd67b4619f
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-12-25 13:13:26 -0500

    update

--- rcm/tag-termtabs/config/fish/functions/t.fish
+++
@@ -1,5 +0,0 @@
-
-function t --wraps tmux --description 'old wrapper around tmx. do not use this'
-  echo "use z instead" 1>&2
-  exit 1
-end
--- rcm/tag-termtabs/config/fish/termtabs-config.fish
+++
@@ -1,5 +0,0 @@
-# vim: set syntax=fish:
-
-if test -n "$TMUX"; or not status --is-interactive
-  exit 0
-end
