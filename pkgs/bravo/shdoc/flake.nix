5a2fbc31ab81df50d8e12d639259f1706c74deed
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2025-12-30 10:02:32 -0500

    update

---
+++ pkgs/bravo/shdoc/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "shdoc - documentation generator for shell scripts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/d981d41ffe5b541eae3782029b93e2af5d229cc2";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/09eb77e94fa25202af8f3e81ddc7353d9970ac1b";
+    utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-stable,
+      utils,
+    }:
+    utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ]
+      (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            pname = "shdoc";
+            version = "1.2";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "reconquest";
+              repo = "shdoc";
+              rev = "v1.2";
+              sha256 = "sha256-oBOXeISPv43VgE6bzPzr6BvVfFwDo1Wx7ekp07w9h6s=";
+            };
+
+            dontBuild = true;
+
+            buildInputs = [ pkgs.gawk ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+
+              cp shdoc $out/bin/
+              cp ${./shdoc-fish_completion} $out/bin/shdoc-fish_completion
+
+              wrapProgram $out/bin/shdoc --prefix PATH : ${pkgs.gawk}/bin
+
+              # wrapProgram $out/bin/shdoc-fish_completion --prefix PATH : ${pkgs.gawk}/bin
+            '';
+          };
+        }
+      );
+}
