18ee6bdf04a5bfd6fed6518e44de27e8160f7535
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-02-14 12:44:18 -0500

    remove z.fish and its completions

--- rcm/config/fish/completions/z.fish
+++
@@ -1,5 +0,0 @@
-complete \
-  --command z \
-  --no-files \
-  --keep-order \
-  --arguments "(z-completions)"
--- rcm/config/fish/functions/z.fish
+++
@@ -1,3 +0,0 @@
-function z --description 'attach to or create a zmx session for a given directory'
-  command z $argv
-end
