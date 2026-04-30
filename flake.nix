0361d076b489ab5bcaae1bbbb33d8d0c9682bbd4
Sasha F <cornflake.matchbox397@icantbelieveitsnotgmail.com>
2026-04-30 07:50:23 -0400

    flake: drop unused ess-ess-hatch input
    The ess-ess-hatch flake was declared as an input but never consumed
    anywhere in eng. Removing the dead reference; ess-ess-hatch is being
    archived.
    
    :clown:

--- flake.nix
+++ flake.nix
@@ -58,12 +58,6 @@
       inputs.nixpkgs-master.follows = "nixpkgs-master";
       inputs.utils.follows = "utils";
     };
-    ess-ess-hatch = {
-      url = "github:amarbel-llc/ess-ess-hatch";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.nixpkgs-master.follows = "nixpkgs-master";
-      inputs.utils.follows = "utils";
-    };
     madder = {
       url = "github:amarbel-llc/madder";
       inputs.nixpkgs.follows = "nixpkgs-amarbel";
